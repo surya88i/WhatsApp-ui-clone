7b6f822bb4688e0a4426ef0c134bd7b32bb5122a
sunil shedge <51407211+surya88i@users.noreply.github.com>
2020-05-05 10:42:43 +0530

    Update ChatScreen.dart

--- lib/pages/ChatScreen.dart
+++ lib/pages/ChatScreen.dart
@@ -9,37 +9,40 @@ class ChatScreen extends StatefulWidget {
 class _ChatsScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: chatsData.length,
-      itemBuilder: (context,index){
-        return Column(
-          children: <Widget>[
-            Divider(height: 0.1),
-            ListTile(
-              onTap: (){},
-              leading: CircleAvatar(
-                backgroundColor: Colors.grey,
-                backgroundImage:AssetImage(chatsData[index].pic),
+    return Scaffold(
+          body: ListView.builder(
+            scrollDirection: Axis.vertical,
+        padding: EdgeInsets.zero,
+        itemCount: chatsData.length,
+        itemBuilder: (context,index){
+          return Column(
+            children: <Widget>[
+              Divider(height: 0.1),
+              ListTile(
+                onTap: (){},
+                leading: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  backgroundImage:AssetImage(chatsData[index].pic),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                      Text(chatsData[index].name,
+                      style: TextStyle(fontSize: 13.0,fontWeight: FontWeight.w400),
+                      ),
+                      Text(chatsData[index].time,
+                      style: TextStyle(fontSize: 13.0,fontWeight: FontWeight.w400),
+                      ),
+                  ],
+                ),
+                subtitle: Text(chatsData[index].msg,
+                style: TextStyle(fontSize: 13.0,fontWeight: FontWeight.w400),
+                ),
               ),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                    Text(chatsData[index].name,
-                    style: TextStyle(fontSize: 13.0,fontWeight: FontWeight.w400),
-                    ),
-                    Text(chatsData[index].time,
-                    style: TextStyle(fontSize: 13.0,fontWeight: FontWeight.w400),
-                    ),
-                ],
-              ),
-              subtitle: Text(chatsData[index].msg,
-              style: TextStyle(fontSize: 13.0,fontWeight: FontWeight.w400),
-              ),
-            ),
-          ],
-        );
-      },
+            ],
+          );
+        },
+      ),
     );
   }
 }
